75b2e98b3d4cd8506bef3099f009143557807f1e
Siddhant shrestha <sidstha58@gmail.com>
2023-12-01 22:14:16 +0545

    class with interface and extend methods

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
 // Basic Datatype
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var id = 5;
-var Name = 'sid';
+var Name = "sid";
 var isBoy = true;
-var x = 'Hello world';
+var x = "Hello world";
 var numbers = [1, 2, 3, 4, 5];
-var arr = [1, 'wis', 3, true];
+var arr = [1, "wis", 3, true];
 //Tuple
 //should be in same order as assigned
-var person = [1, 'sid', true];
+var person = [1, "sid", true];
 //Tuple array
 var product;
 product = [
-    [1, 'apple'],
-    [2, 'bag'],
-    [2, 'rack'],
+    [1, "apple"],
+    [2, "bag"],
+    [2, "rack"],
 ];
 //Union
 // if you have to assign two diff value
 var pid;
-pid = 'apple';
+pid = "apple";
 // Enum
 var Keys1;
 (function (Keys1) {
@@ -27,7 +42,7 @@ var Keys1;
     Keys1[Keys1["left"] = 2] = "left";
     Keys1[Keys1["right"] = 3] = "right";
 })(Keys1 || (Keys1 = {}));
-console.log(Keys1.left);
+// console.log(Keys1.left);
 var Keys2;
 (function (Keys2) {
     Keys2["up"] = "up";
@@ -35,4 +50,53 @@ var Keys2;
     Keys2["left"] = "left";
     Keys2["right"] = "right";
 })(Keys2 || (Keys2 = {}));
-console.log(Keys2.right);
+var user = {
+    id: 1,
+    name: "Sid",
+};
+//Type assertion
+var nid = 1;
+// let cid= <number>nid
+var cid = nid;
+cid = 2;
+//function
+function addNum(x, y) {
+    return x + y;
+}
+//  console.log(addNum(10,2));
+function smth(msg) {
+    console.log(msg);
+}
+smth("This is null");
+var users = {
+    id: 12,
+    name: "Sid",
+};
+var ID = "DUKE" || 2;
+var add = function (x, y) { return x + y; };
+console.log("Add:", add(4, 3));
+var multiply = function (x, y) { return x * y; };
+console.log("Multiply:", multiply(4, 3));
+var Car = /** @class */ (function () {
+    function Car(model, name) {
+        this.model = model;
+        this.name = name;
+    }
+    Car.prototype.buy = function () {
+        return "".concat(this.name, " model ").concat(this.model, " obtained!!!");
+    };
+    return Car;
+}());
+var Company = /** @class */ (function (_super) {
+    __extends(Company, _super);
+    function Company(model, name, company) {
+        var _this = _super.call(this, id, name) || this;
+        _this.company = company;
+        return _this;
+    }
+    return Company;
+}(Car));
+var gadi = new Company(2001, "supra", "Honda");
+console.log(gadi);
+console.log(gadi.buy());
+//sub class
--- src/index.ts
+++ src/index.ts
@@ -1,127 +1,150 @@
 // Basic Datatype
 
-let id:number = 5
-let Name:string = 'sid'
+let id: number = 5
+let Name: string = "sid"
 let isBoy: boolean = true
-let x:any ='Hello world'
+let x: any = "Hello world"
 
-let numbers: number[] = [1,2,3,4,5]
+let numbers: number[] = [1, 2, 3, 4, 5]
 
-let arr: any[] = [1, 'wis',3, true]
- 
+let arr: any[] = [1, "wis", 3, true]
+
 //Tuple
 //should be in same order as assigned
-let person: [number, string , boolean] = [ 1, 'sid', true]
+let person: [number, string, boolean] = [1, "sid", true]
 
 //Tuple array
-let product: [number,string][]
+let product: [number, string][]
 
-product =[
-    [1,'apple'],
-    [2,'bag'],
-    [2,'rack'],
+product = [
+  [1, "apple"],
+  [2, "bag"],
+  [2, "rack"],
 ]
 
 //Union
 // if you have to assign two diff value
 
 let pid: string | number
-pid = 'apple'
+pid = "apple"
 
 // Enum
-enum Keys1{
-    up,
-    down,
-    left,
-    right
+enum Keys1 {
+  up,
+  down,
+  left,
+  right,
 }
 
-console.log(Keys1.left);
-enum Keys2{
-    up= 'up',
-    down = 'down',
-    left='left',
-    right='right'
+// console.log(Keys1.left);
+enum Keys2 {
+  up = "up",
+  down = "down",
+  left = "left",
+  right = "right",
 }
 
-console.log(Keys2.right);
+// console.log(Keys2.right);
 
 //Objects
 
 type User = {
-    id: number
-    name:string
+  id: number
+  name: string
 }
 
-const user: User={
-    id:1,
-    name:'Sid'
+const user: User = {
+  id: 1,
+  name: "Sid",
 }
 
 //Type assertion
 
-let nid:number=1
+let nid: number = 1
 // let cid= <number>nid
-let cid= nid as number
+let cid = nid as number
 
 cid = 2
 
 //function
 
- function addNum(x: number, y: number){
-    return x + y
- }
-
- console.log(addNum(10,2));
-
- function smth(msg: string|number):void{
-    console.log(msg);
- }
-
- smth('This is null')
-
- //Interface
- // instead of type, interface is prefer but 
- // it cannot be used with primitive and union
-
- interface Users{
-    id: number
-    name:string
-    //optional condition if age is not present jj
-    age?: number
+function addNum(x: number, y: number) {
+  return x + y
 }
 
-const users: Users={
-    id:12,
-    name:'Sid'
+//  console.log(addNum(10,2));
+
+function smth(msg: string | number): void {
+  console.log(msg)
+}
+
+smth("This is null")
+
+//Interface
+// instead of type, interface is prefer but
+// it cannot be used with primitive and union
+
+interface Users {
+  id: number
+  name: string
+  //optional condition if age is not present jj
+  age?: number
+}
+
+const users: Users = {
+  id: 12,
+  name: "Sid",
 }
 
 // interface ID = number | string cause error
 type ID = number | string
-const ID = 'DUKE' || 2
+const ID = "DUKE" || 2
 
 //interface with function
 
 interface mathFunc {
-    (x: number, y:number): number
+  (x: number, y: number): number
 }
 
-const add: mathFunc = (x:number,y:number):number => x+y
-console.log('Add:',add(4,3));
-const multiply: mathFunc = (x:number,y:number):number => x*y
-console.log('Multiply:',multiply(4,3));
+const add: mathFunc = (x: number, y: number): number => x + y
+console.log("Add:", add(4, 3))
+const multiply: mathFunc = (x: number, y: number): number => x * y
+console.log("Multiply:", multiply(4, 3))
 
 //Classes
 
-class Car{
-    model: number
-    name: string
-
-    constructor(model:number, name:string){
-        this.model=model
-        this.name= name
-    }
+interface CarInterface {
+  model: number
+  name: string
+  buy(): string
 }
 
-const honda = new Car(2001,'supra')
+class Car implements CarInterface {
+  model: number
+  name: string
 
+  constructor(model: number, name: string) {
+    this.model = model
+    this.name = name
+  }
+
+  buy() {
+    return `${this.name} model ${this.model} obtained!!!`
+  }
+}
+
+class Company extends Car {
+  company: string
+  constructor(model: number, name: string, company: string) {
+    super(id, name)
+    this.company = company
+  }
+}
+
+
+const gadi = new Company(2001, "supra","Honda")
+
+console.log(gadi);
+console.log(gadi.buy())
+
+//sub class
